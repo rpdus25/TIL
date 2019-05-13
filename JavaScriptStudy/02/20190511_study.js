bababd9b2df05efe6ec5a4456d8d68e5ef782c71
rpdus25 <rpdus25@naver.com>
2019-05-13 23:12:58 +0900

    'js30_10_check_study

---
+++ JavaScriptStudy/02/20190511_study.js
@@ -0,0 +1,16 @@
+const gyeyeon = {
+    age : 28,
+    bloodType : 'A',
+    height : 164,
+    country : 'Korea',
+    job: 'Web Publisher',
+    study: function () {
+        return '자바스크립트를 공부중입니다.';
+    },
+    favorite: function () {
+        return '딸기를 좋아합니다';
+    }
+}
+
+
+
